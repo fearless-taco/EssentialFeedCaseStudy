6e5923191f24d71166171e3cfd9d5a1248296e75
Jason Kuan <jsonkuan@icloud.com>
2023-02-07 20:18:42 +0100

    Assert first images exists

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,13 +8,16 @@
 import XCTest
 
 final class EssentialAppUIAcceptanceTests: XCTestCase {
-  
-  func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() throws {
-    let app = XCUIApplication()
     
-    app.launch()
-    
-    let feedCells = app.cells.matching(identifier: "feed-image-cell")
-    XCTAssertEqual(feedCells.count, 22)
-  }
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() throws {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+        XCTAssertEqual(feedCells.count, 22)
+        
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
+        XCTAssertTrue(firstImage.exists)
+    }
 }
